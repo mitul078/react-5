fd689c03dfb594ed6eaac9d9a593228d263e458e
mitul078 <mituljodhani788@gmail.com>
2025-06-11 10:18:26 +0530

    Connect frontend to backend API

--- frontend/api/axiosConfig.jsx
+++ frontend/api/axiosConfig.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL : 'http://localhost:3000/'
+    baseURL : 'https://backend-1vk9.onrender.com/'
 })
 
 export default instance;
